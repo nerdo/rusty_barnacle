fb344eb42a56b282404db9a9acc62af1e420d606
Dannel Albert <cuebix@gmail.com>
2022-11-27 14:12:12 -0500

    refactor: add Config struct scaffolding

--- src/lib.rs
+++ src/lib.rs
@@ -8,9 +8,15 @@ pub trait FileIO {
     fn exists<P: AsRef<Path> + 'static>(&self, path: P) -> bool;
 }
 
-pub fn find_nearest(file_io: &impl FileIO) -> String {
-    println!("file_io.exists('/') = {}", file_io.exists("/"));
+pub fn find_nearest<F: FileIO>(config: &Config<F>) -> String {
+    println!("file_io.exists('/') = {}", config.file_io.exists("/"));
     String::new()
+}
+
+pub struct Config<'a, F: FileIO> {
+    file_io: &'a F,
+    target_paths: Vec<&'a str>,
+    starting_path: &'a str,
 }
 
 #[cfg(test)]
@@ -22,7 +28,13 @@ mod tests {
         let mut file_io = MockFileIO::new();
         file_io.expect_exists::<&str>().return_const(true);
 
-        let result = find_nearest(&file_io);
+        let config = Config {
+            file_io: &file_io,
+            target_paths: vec!["node_modules"],
+            starting_path: "/home/user/code/project"
+        };
+
+        let result = find_nearest(&config);
 
         assert_eq!(result, "");
     }
