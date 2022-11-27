a5f363b5e274d643f757e863a3035d031dd36e03
Dannel Albert <cuebix@gmail.com>
2022-11-27 15:31:11 -0500

    refactor: remove generic from FileIO trait

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,29 @@
 use mockall::*;
 use mockall::predicate::*;
-
-use std::{path::Path, convert::AsRef};
+use std::path::Path;
 
 #[automock]
 pub trait FileIO {
-    fn exists<P: AsRef<Path> + 'static>(&self, path: P) -> bool;
+    fn exists(&self, path: &str) -> bool;
 }
 
 pub fn find_nearest<F: FileIO>(config: &Config<F>) -> String {
-    println!("file_io.exists('/') = {}", config.file_io.exists("/"));
-    String::new()
+    let mut cur_path = Path::new(config.starting_path);
+
+    loop {
+        for p in config.target_paths.iter() {
+            if let Some(test_path) = cur_path.join(p).to_str() {
+                if config.file_io.exists(&test_path) {
+                    return String::from(test_path);
+                }
+            }
+        }
+
+        cur_path = match cur_path.parent() {
+            Some(parent) => parent,
+            None => return String::new()
+        };
+    }
 }
 
 pub struct Config<'a, F: FileIO> {
@@ -26,7 +39,7 @@ mod tests {
     #[test]
     fn it_works() {
         let mut file_io = MockFileIO::new();
-        file_io.expect_exists::<&str>().return_const(true);
+        file_io.expect_exists().return_const(true);
 
         let config = Config {
             file_io: &file_io,
