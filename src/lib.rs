661180d261a81b59d8bda797122209c81d93b5a8
Dannel Albert <cuebix@gmail.com>
2022-11-28 18:43:17 -0500

    refactor: return Option<String> in find_nearest

--- src/lib.rs
+++ src/lib.rs
@@ -7,7 +7,7 @@ pub trait FileIO {
     fn exists(&self, path: &str) -> bool;
 }
 
-pub fn find_nearest<F: FileIO>(config: &Config<F>) -> String {
+pub fn find_nearest<F: FileIO>(config: &Config<F>) -> Option<String> {
     let init_path_buf = Path::new(config.starting_path).join("_");
     let mut cur_path = init_path_buf.as_path();
 
@@ -17,13 +17,13 @@ pub fn find_nearest<F: FileIO>(config: &Config<F>) -> String {
         for p in config.target_paths.iter() {
             if let Some(test_path) = cur_path.join(p).to_str() {
                 if config.file_io.exists(&test_path) {
-                    return String::from(test_path);
+                    return Some(String::from(test_path));
                 }
             }
         }
     }
 
-    String::new()
+    None
 }
 
 pub struct Config<'a, F: FileIO> {
@@ -35,6 +35,22 @@ pub struct Config<'a, F: FileIO> {
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn it_returns_none_when_it_does_not_find_a_target_path() {
+        let mut file_io = MockFileIO::new();
+        file_io.expect_exists().return_const(false);
+
+        let config = Config {
+            file_io: &file_io,
+            target_paths: vec!["node_modules/.bin", ".custom_bin_path"],
+            starting_path: "/home/user/code/project/src/app/components"
+        };
+
+        let result = find_nearest(&config);
+
+        assert_eq!(result, None);
+    }
 
     #[test]
     fn it_finds_the_nearest_path_where_target_paths_exist() {
@@ -49,6 +65,6 @@ mod tests {
 
         let result = find_nearest(&config);
 
-        assert_eq!(result, "/home/user/code/project/node_modules/.bin");
+        assert_eq!(result, Some(String::from("/home/user/code/project/node_modules/.bin")));
     }
 }
